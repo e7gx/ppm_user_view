eb89f825506cea144e8faa13297aceb66b97a6f1
e7gx <abalgabo@gmail.com>
2024-01-26 07:27:44 +0300

    .

--- lib/api/chatgpt_api.dart
+++
@@ -1,3 +0,0 @@
-class APIKey {
-  static const apikey = "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";
-}
--- lib/page/chat_main_page.dart
+++ lib/page/chat_main_page.dart
@@ -1,8 +1,8 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
-import 'package:hello_world/api/chatgpt_api.dart';
+// import 'package:hello_world/api/chatgpt_api.dart';
 import 'package:http/http.dart' as http;
-// import 'package:hello_world/.env';
+import 'package:hello_world/.env';
 
 class MainPage extends StatefulWidget {
   const MainPage({super.key});
